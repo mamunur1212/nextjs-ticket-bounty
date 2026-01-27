b33e86b942fbce604f915d4c2d5986e2520928d9
mamun <mamun.ruet.ete19@gmail.com>
2026-01-27 07:47:39 +0100

    on demanding caching

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,8 +3,6 @@ import { Heading } from '@/components/heading';
 import { Spinner } from '@/components/spinner';
 import { TicketList } from '@/features/ticket/components/ticket-list';
 
-export const revalidate = 30;
-
 const TicketsPage = async () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { db } from '@/lib/db';
 import { ticketsPath } from '@/paths';
@@ -8,5 +9,7 @@ export const deleteTicket = async (id: string) => {
   await db.ticket.delete({
     where: { id },
   });
+
+  revalidatePath(ticketsPath())
   redirect(ticketsPath());
 };
